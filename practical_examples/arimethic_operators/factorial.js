6e4bb4dd24a1496fcf12cdf99acdc4490d35884c
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-22 20:53:44 +0100

    Add new examples of arimethic operators

---
+++ practical_examples/arimethic_operators/factorial.js
@@ -0,0 +1,17 @@
+//Task
+//Implement a function named factorial that has one parameter: an integer.
+// It must return the value of  (i.e.,  factorial).
+//Input Format
+//Locked stub code in the editor reads a single integer, , from stdin and passes it to a function named factorial.
+//Constraints : 1 <= n <= 10
+//Output Format
+// The function must return the value of n!
+function factorial(n){
+let result=1;
+for (let i=1; i<= n; i++){
+    result *= i;
+}
+return result;
+}
+
+
