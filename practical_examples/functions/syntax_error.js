db472ea7f2788b1ad691b1d3a2cb33a2b208df51
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:11:03 +0100

    Add syntax error exercise

---
+++ practical_examples/functions/syntax_error.js
@@ -0,0 +1,10 @@
+//Error in syntax:
+// function cubes(a) {
+//     retunr a ** 3;
+// }
+// The issue here is the syntax for the word retunr
+// Fixing the issue:
+function cubes(a) {
+    return a ** 3;
+}
+console.log(cubes(3));
