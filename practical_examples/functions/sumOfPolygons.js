38023c59ea65368400675200fa52795d7f18d865
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:10:24 +0100

    Add exercise about the addition of polygons

---
+++ practical_examples/functions/sumOfPolygons.js
@@ -0,0 +1,9 @@
+//Exercise: Sum of Polygons
+//Remember as we need to have the results
+// to use the return keyword 
+function sumPolygonAngles(n){
+    return (n-2)*180;
+}
+console.log(sumPolygonAngles(3));
+console.log(sumPolygonAngles(4));
+console.log(sumPolygonAngles(6));
