cc68c941cad89f169b7ad8e8b0bdcf901dcbea0f
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:08:10 +0100

    Add exercise about calculating a value of two parameters in javascript

---
+++ practical_examples/functions/powerCalculator.js
@@ -0,0 +1,10 @@
+// Create a Function that calculates the power between 
+// the voltage and the current
+// It is a function remember to use the keyword return
+// to get the result !!!
+function circuitPower(voltage, current){
+    return voltage * current;
+}
+console.log(circuitPower(10,30));
+console.log(circuitPower(80,20));
+console.log(circuitPower(55,11));
