49bc0c1bca687b4c5e2ce66c76423a765b15a13f
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:06:59 +0100

    Add exercise about converting minutes to seconds

---
+++ practical_examples/functions/minutes_to_seconds.js
@@ -0,0 +1,9 @@
+// Function to convert minutes to seconds
+function convert(minutes){
+    return minutes * 60;
+}
+console.log(convert(5));
+console.log(convert(3));
+console.log(convert(2));
+// The return keyword is really important always!!! especially in a
+// function!
