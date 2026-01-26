062b40cc37ed6711e84539e6e9a83dbef0a96bfe
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:08:46 +0100

    Add exercise of strings and concatenation

---
+++ practical_examples/functions/returnSomethingToMe.js
@@ -0,0 +1,8 @@
+// Write a function that returns the string "something" joined with a space
+// and the given argument a
+// Always remember to use the return keyword!
+function giveMeSomething(a) {
+    return "something" + " " + a;
+}
+console.log(giveMeSomething("is better than nothing"));
+console.log(giveMeSomething("looks like a small thing but it is a big thing!"));
