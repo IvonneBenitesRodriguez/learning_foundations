2ef178b802d8f3b03e813989034798e6ed6c852e
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:04:54 +0100

    Add easy exercise of addition

---
+++ practical_examples/functions/addition.js
@@ -0,0 +1,7 @@
+// Very Easy : addition:
+// Always use the return keyword
+function addition(a, b){
+    return a + b;
+}
+console.log(addition(10, 20.3));
+// the keyword return is pretty important!!!
