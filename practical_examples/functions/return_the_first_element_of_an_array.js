a8c05d3fff196ab92750754a978842d7e7964d2a
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-26 18:09:24 +0100

    Add exercise using arrays and index position

---
+++ practical_examples/functions/return_the_first_element_of_an_array.js
@@ -0,0 +1,7 @@
+// This function return the first element of an array
+// remember to use the keyword return
+// remember that in an array the position 0 of the index is the first element!
+function getFirstValue(arr){
+    return arr[0];
+}
+console.log(getFirstValue([12,11,10]));
