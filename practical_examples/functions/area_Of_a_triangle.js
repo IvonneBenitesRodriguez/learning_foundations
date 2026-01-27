637b5f23de6df0e4e417f6642bdaaf0bef58f7a9
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-27 12:24:04 +0100

    Add exercise about the area of a triangle

---
+++ practical_examples/functions/area_Of_a_triangle.js
@@ -0,0 +1,12 @@
+// Get the area of a triangle
+// Write a function that takes the base and height of a triangle
+// please return its area.
+
+//Solution:
+function area (base, height){
+    return (base * height)/2;
+}
+
+console.log(area(10,20));
+console.log(area(23,32));
+console.log(area(4,5));
