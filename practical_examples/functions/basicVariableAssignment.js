3f4980cadf51a0e51bbf48d44104d438e3ae0a21
Ivonne Benites <ivonnebenites2015@gmail.com>
2026-01-27 12:24:37 +0100

    Add exercise about variable assignement

---
+++ practical_examples/functions/basicVariableAssignment.js
@@ -0,0 +1,17 @@
+// Fix the code 
+// Exercise : Basic Variable Assignment:
+// A student learning js was trying to make a function. His code should 
+// concatenate a passed string name with string "edabit" and store it
+// in a variable called result
+
+// Solution:
+
+function nameString(name) {
+    var b = "Edabit";
+    var result = name + b;
+    return result;
+}
+console.log(nameString("javascript"));
+console.log(nameString("nice day"));
+
+
